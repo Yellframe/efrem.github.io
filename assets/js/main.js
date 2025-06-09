8dd287c00e8b22a1315bf36648e7e7803a4d71a3
Yellframe <162080155+Yellframe@users.noreply.github.com>
2025-06-09 15:16:34 +0300

    test

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/* Основные стили */
+body {
+  font-family: 'Georgia', serif;
+  color: #555555;
+  background: white;
+  margin: 0;
+  padding: 20px;
+  line-height: 1.6;
+}
+
+.header {
+  text-align: center;
+  margin: 40px 0;
+}
+
+.header h1 {
+  font-size: 1.75rem; /* ~28px */
+  font-weight: 600;
+  margin-bottom: 10px;
+}
+
+/* Добавьте остальные стили сюда */
